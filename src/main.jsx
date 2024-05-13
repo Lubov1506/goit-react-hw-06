a4d3f2bfc9f0133f26a685ee7e1bab990de05199
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-05-13 22:06:48 +0300

    add localStorage

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,15 @@ import App from "./App.jsx";
 import "modern-normalize";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import createStoreWithPersist from "./redux/store.js";
+
+const { store, persistor } = createStoreWithPersist();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers, createStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { contactsReducer } from "./contactsSlice";
 import { filtersReducer } from "./filtersSlice";
 
-export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-    filters: filtersReducer,
-  },
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filters: filtersReducer,
 });
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default () => {
+  let store = createStore(persistedReducer);
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
